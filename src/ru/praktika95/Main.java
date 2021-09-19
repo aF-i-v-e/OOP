7fed0c690a5c4f2a82e9d6b5178620cb325d4101
aF-i-v-e <avelichko.ru@yandex.ru>
2021-09-19 20:21:35 +0500

    my main class from lesson

--- src/ru/praktika95/Main.java
+++ src/ru/praktika95/Main.java
@@ -1,2 +1,33 @@
-package ru.praktika95;public class Main {
+package ru.praktika95;
+
+public class Main {
+    private String str="af";
+    private final String str2="5";//это типа константы
+    private static final String constStr="str";
+    private volatile String str5="многопоточность";
+    private  transient String str6="сериализация для долгого сохранения в памяти или передачи данных";
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a=0;
+        long h=9;
+        char ch='a';
+        boolean g=true;
+        var c=9;
+        float r=78.0f;
+        String s="stroka";
+        method(a);
+        if (true) {
+
+        }
+        for (int i=0;i<5;i++){
+            System.out.print(s);
+        }
+    }
+    private static void method(int a){
+        a=77;
+    }
+    void m(){
+        //данный метод доступен всему пакету
+        //буз модификатора доступа видны всему пакету
+    }
 }

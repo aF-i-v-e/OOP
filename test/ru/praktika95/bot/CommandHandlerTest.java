73d002b0ddced7284511fb79f53788b35b550994
aF-i-v-e <avelichko.ru@yandex.ru>
2021-09-25 14:17:47 +0500

    rename botAnswer to botRequest and change output value in tests

--- test/ru/praktika95/bot/CommandHandlerTest.java
+++ test/ru/praktika95/bot/CommandHandlerTest.java
@@ -16,49 +16,49 @@ class CommandHandlerTest {
     @Test
     void testCorrectHelpMessage() {
         String correctAnswer = "Доступные команды:\nhelp - узнать список доступных команд.\nhello - получить приветственное сообщение.\nshow - узнать ближайшие мероприятия.\nchoose \"номер мероприятия\" - выбрать мероприятие.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"help", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"help", "0"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 
     @Test
     void testCorrectHelloMessage() {
         String correctAnswer = "Привет!\nЯ бот, которые может показать ближайшие мероприятия. Вы можете подписаться на их уведомление и вы точно про него не забудете.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"hello", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"hello", "0"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 
     @Test
     void testCorrectShowMessage() {
         String correctAnswer = "1. Первое мероприятие.\n2. Второе мероприятие.\n3. Третье мероприятие.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"show", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"show", "0"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 
     @Test
     void testCorrectChooseMessage() {
         String correctAnswer = "Информация второго мероприятия.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"choose", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"choose", "2"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 
     @Test
     void testNotExistsNumberEventChooseMessage() {
         String correctAnswer = "Такого мероприятия не существует.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"choose", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"choose", "0"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 
     @Test
     void testIncorrectNumberEventMessage() {
         String correctAnswer = "Вы ввели некорректный номер мероприятия.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"choose", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"choose", "-1"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 
     @Test
     void testIncorrectMessage() {
         String correctAnswer = "Введённой команды не существует, вы можете выполнить команду help, чтобы узнать список доступных команд.";
-        String botAnswer = commandHandler.commandHandler(new String[] {"read", "0"});
-        assertEquals(correctAnswer, botAnswer);
+        BotRequest botRequest = commandHandler.commandHandler(new String[] {"read", "0"});
+        assertEquals(correctAnswer, botRequest.getMessage());
     }
 }

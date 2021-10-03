201ab255f0a1a99ea0286a5ad5bb037ca1d6c3fa
AntonVolkov000 <68469591+AntonVolkov000@users.noreply.github.com>
2021-10-03 21:44:04 +0500

    Revert "Anton"

--- src/main/java/ru/praktika95/bot/App.java
+++ src/main/java/ru/praktika95/bot/App.java
@@ -5,7 +5,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class App {
 
     public static void main(String[] args) throws TelegramApiException {
-        Bot bot = new Bot("EventsEkbBot", "");
+        Bot bot = new Bot("AlisaRushBot", "2042186727:AAHrOhFORwT88bSuqZbqj6ly4CXoqoRt37g");
         bot.botConnect();
     }
 }

07d17018bb0f922ae5d267dceea93595643c48d6
aF-i-v-e <avelichko.ru@yandex.ru>
2021-10-09 14:07:08 +0500

    Change BotRequest, in Bot sends BotRequest, BotRequestHandler sends typeButtons and botCommand

--- src/main/java/ru/praktika95/bot/BotRequest.java
+++ src/main/java/ru/praktika95/bot/BotRequest.java
@@ -3,19 +3,18 @@ package ru.praktika95.bot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 public class BotRequest {
     String chatId;
-    String inputText;
-    String command;
-    int status;
-    List<List<InlineKeyboardButton>> buttons;
+    String typeButtons;
+    String button;
+    String botCommand;
+    Map<String,Integer> map;
 
     public BotRequest(Update update){
         chatId = update.getMessage().getChatId().toString();
-        inputText = update.getMessage().getText();
-        buttons = new ArrayList<>();
+        map = new HashMap<String, Integer>();
     }
 }
--- src/main/java/ru/praktika95/bot/BotRequestHandler.java
+++ src/main/java/ru/praktika95/bot/BotRequestHandler.java
@@ -3,7 +3,7 @@ package ru.praktika95.bot;
 public class BotRequestHandler {
     public BotResponse getBotAnswer(BotRequest botRequest) {
         CommandHandler comH = new CommandHandler();
-        BotResponse botResponse = comH.commandHandler(botRequest.inputText);
+        BotResponse botResponse = comH.commandHandler(botRequest.typeButtons, botRequest.botCommand);
         botResponse.setChatId(botRequest.chatId);
         return botResponse;
     }

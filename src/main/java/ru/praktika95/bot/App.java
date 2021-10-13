74a39ab7d8788df50db97c1ee48526529a1239b6
Anton Volkov <xzxz2020.ru@mail.ru>
2021-10-13 22:49:41 +0500

    connect DB

--- src/main/java/ru/praktika95/bot/App.java
+++ src/main/java/ru/praktika95/bot/App.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class App {
@@ -13,14 +14,18 @@ public class App {
             FileInputStream config = new FileInputStream("src/main/resources/config.properties");
             Properties property = new Properties();
             property.load(config);
-
             String name = property.getProperty("bot.name");
             String token = property.getProperty("bot.token");
             Bot bot = new Bot(name, token);
             bot.botConnect();
-
-        } catch (IOException e) {
-            System.err.println("Файл свойств отсутствует!");
+            String urlBase = property.getProperty("db.host");
+            String userBase = property.getProperty("db.login");
+            String passBase = property.getProperty("db.password");
+            DataBase dataBase = new DataBase();
+            dataBase.connect(urlBase, userBase, passBase);
+            dataBase.createTable();
+        } catch (IOException | SQLException e) {
+            System.err.println(e);
         }
     }
 }

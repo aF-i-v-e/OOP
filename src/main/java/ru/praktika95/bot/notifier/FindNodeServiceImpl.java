0f539f6332e5f33a97c81f663f9f28aea96e55b0
aF-i-v-e <avelichko.ru@yandex.ru>
2021-12-02 09:57:32 +0500

    update with Feodor FindNodeServiceImpl

--- src/main/java/ru/praktika95/bot/notifier/FindNodeServiceImpl.java
+++ src/main/java/ru/praktika95/bot/notifier/FindNodeServiceImpl.java
@@ -1,12 +1,15 @@
 package ru.praktika95.bot.notifier;
 
+import org.springframework.stereotype.Component;
 import ru.praktika95.bot.App;
 import ru.praktika95.bot.Event;
 import ru.praktika95.bot.Response;
 import ru.praktika95.bot.service.DataBaseWorkService;
+
+import javax.persistence.Column;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
+@Component
 public class FindNodeServiceImpl implements FindNodeService {
     @Override
     public void findNode(String date) {
